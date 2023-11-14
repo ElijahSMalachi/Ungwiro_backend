dcb994976c45eac6119a7972cf8ff7ddcde207ff
Elijah S Malachi <121290719+ElijahSMalachi@users.noreply.github.com>
2023-11-13 17:27:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
+// import cors from "cors";
+// import dotenv from "dotenv";
 // import multer from "multer";
-import helmet from "helmet";
-import morgan from "morgan";
+// import helmet from "helmet";
+// import morgan from "morgan";
 // import path from "path";
 // import { fileURLToPath } from "url";
-import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/users.js";
+// import authRoutes from "./routes/auth.js";
+// import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 // import commentRoutes from "./routes/comments.js";
 // import chatRoutes from "./routes/chat.js";
 // import replyRoutes from "./routes/reply.js";
 // import replyToReplyRoutes from "./routes/replyToReply.js";
 // import messageRoutes from "./routes/message.js";
-import { register } from "./controllers/auth.js";
+// import { register } from "./controllers/auth.js";
 // import { createPost } from "./controllers/posts.js";
 // import { verifyToken } from "./middleware/auth.js";
 // import { addCoverPhoto, updateProfilePicture } from "./controllers/users.js";
@@ -24,15 +24,15 @@ import { register } from "./controllers/auth.js";
 /* CONFIGURATIONS */
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
-dotenv.config();
+// dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
+// app.use(helmet());
+// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+// app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+// app.use(cors());
 // app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
@@ -47,7 +47,7 @@ app.use(cors());
 // const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
-app.post("/auth/register",  register);
+// app.post("/auth/register",  register);
 // app.post("/auth/register", upload.single("picture"), register);
 // app.post("/posts", verifyToken, upload.single("picture"), createPost);
 // app.post("/posts", verifyToken, upload.single("picture"), createPost);
@@ -55,8 +55,8 @@ app.post("/auth/register",  register);
 // app.patch("/users/:userId/profilepicture", upload.single("picture"), updateProfilePicture);
 
 /* ROUTES */
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
+// app.use("/auth", authRoutes);
+// app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 // app.use("/comment", commentRoutes);
 // app.use("/reply", replyRoutes);
